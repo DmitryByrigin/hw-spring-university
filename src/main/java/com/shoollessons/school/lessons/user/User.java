fcbc558e67e9213ddeabe591dfe32d3bea9c8c6c
Aspergillusplay <kartofka123@i.ua>
2024-02-21 19:45:12 +0200

    Create user model

---
+++ src/main/java/com/shoollessons/school/lessons/user/User.java
@@ -0,0 +1,15 @@
+package com.shoollessons.school.lessons.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}

4568b484fcb5a27646e3d0e159833141be87368d
Aspergillusplay <kartofka123@i.ua>
2024-03-02 13:51:44 +0200

    Make test for creating borrowings

--- src/main/java/com/schoollessons/school/lessons/borrowings/CreateBorrowingDTO.java
+++ src/main/java/com/schoollessons/school/lessons/borrowings/CreateBorrowingDTO.java
@@ -3,11 +3,12 @@ package com.schoollessons.school.lessons.borrowings;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Builder
 @Data
-
+@NoArgsConstructor
 public class CreateBorrowingDTO {
     private Long customerId;
     private Long bookId;

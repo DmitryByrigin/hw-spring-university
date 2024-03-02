518db314f960a074f2b6c861b19a22361af51d0a
DmitryByrigin <dmitri.d907@gmail.com>
2024-03-02 11:34:42 +0100

    Added unit tests for BookController and created api.adoc

--- src/test/java/com/shoollessons/Shool/lessons/SchoolLessonsApplicationTests.java
+++ src/test/java/com/shoollessons/Shool/lessons/SchoolLessonsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoolLessonsApplicationTests {
+class SchoolLessonsApplicationTests {
 
 	@Test
 	void contextLoads() {
